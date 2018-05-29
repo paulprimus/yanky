5dc119c30050a732de8260219df1d2b4b6bd386a
Paul Primus <paul.pacher@gmail.com>
2018-05-29 20:13:00 +0200

    cursor goto

--- src/main.rs
+++ src/main.rs
@@ -2,86 +2,79 @@
 extern crate termion;
 
 use std::io::{self, Read, Write};
+use termion::cursor;
 
-use termion::raw::{IntoRawMode};
-use termion::cursor::{DetectCursorPos};
-
+const BLANK: &'static u8 = &b'\x20';
+const LINE_BREAK: &'static u8 = &b'\x0a';
 
 
 fn main() -> io::Result<()> {
 
-  //  let all: Vec<Vec<Vec<u8>>> = read_stdin();
-   // write_stdout(&all);
+    let all: Vec<Vec<Vec<u8>>> = read_stdin();
     get_cursor_pos();
+    write_stdout(&all);
     Ok(())
 }
 
-// fn read_stdin() -> Vec<Vec<Vec<u8>>> {
-//     let mut buffer: Vec<u8> = Vec::new();
-//     let stdin = io::stdin();
-//     let mut in_handle = stdin.lock();
+fn read_stdin() -> Vec<Vec<Vec<u8>>> {
+    let mut buffer: Vec<u8> = Vec::new();
+    let stdin = io::stdin();
+    let mut in_handle = stdin.lock();
 
-//     match in_handle.read_to_end(&mut buffer) {
-//         Ok(v) => println!("{}", v),
-//         Err(e) => println!("{}", e),
-//     };
+    match in_handle.read_to_end(&mut buffer) {
+        Ok(v) => println!("{}", v),
+        Err(e) => println!("{}", e),
+    };
 
-//     let mut word: Vec<u8> = Vec::new();
-//     let mut line: Vec<Vec<u8>> = Vec::new();
-//     let mut all: Vec<Vec<Vec<u8>>> = Vec::new();
-//     for i in &buffer {
-//         match i {
-//             b'\x20' => { // space
-//                 let mut blank: Vec<u8> = Vec::new();
-//                 blank.push(b'\x25');
-//                 line.push(blank);
-//                 line.push(word);
-//                 word = Vec::new();                
-//             }
-//             b'\x0a' => { // linefeed              
-//                 all.push(line);
-//                 line = Vec::new();
-//             }
-//             _ => {
-//                 word.push(*i);
-//             }
-//         };
-//     }
-//     return all;
-// }
-
-// fn write_stdout(all: & Vec<Vec<Vec<u8>>>) {
-//     let  stdout = io::stdout();
-//     let mut out_handle = stdout.lock(); 
-   
-//     for line in all {
-//         for word in line {
-//         match out_handle.write(&word) {
-//             Ok(_) => continue,
-//             Err(e) => println!("{}", e),
-//         };
-//         }
-//     }
-// }
-
-#[cfg(unix)]
-fn get_cursor_pos() {
-
-    let mut stdout= match io::stdout().into_raw_mode() {
-        Ok(v) => v,
-        Err(_) => println!("passt nicht"),
-    } ;
-    
-//     {
-//         Ok(stdout) => println!("Stdout into raw mode!"),
-//         Err(e) => println!("Error while trying to access tty in raw mode:\n {}",e)
-//     };
-//    stdout.write("wrieteeeeeeee!!!!");
-     let (x,y) = stdout.cursor_pos().unwrap();
-     println!("x:{},y:{}",x,y );
-   
+    let mut word: Vec<u8> = Vec::new();
+    let mut line: Vec<Vec<u8>> = Vec::new();
+    let mut all: Vec<Vec<Vec<u8>>> = Vec::new();
+    for i in &buffer {
+        word.push(*i);
+        match i {
+            BLANK => { // space
+                // let mut blank_vec: Vec<u8> = Vec::new();
+                // blank_vec.push(*BLANK);
+                // line.push(blank_vec);
+                line.push(word);
+                word = Vec::new();                
+            }
+            LINE_BREAK => { // linebreak  
+                // let mut line_break: Vec<u8> = Vec::new();
+                // line_break.push(*LINE_BREAK);
+                // line.push(line_break);                       
+                all.push(line);
+                line = Vec::new();
+            }
+             _ => {
+                
+            //     word.push(*i);
+            }
+        };
+    }
+    line.push(word);
+    all.push(line);
+    return all;
 }
 
+fn write_stdout(all: & Vec<Vec<Vec<u8>>>) {
+    let  stdout = io::stdout();
+    let mut out_handle = stdout.lock(); 
+    cursor::Goto(4, 2);
+   // write!(out_handle,"{}", cursor::Goto(4, 2));
+    for line in all {
+        for word in line {
+        match out_handle.write(&word) {
+            Ok(_) => continue,
+            Err(e) => println!("{}", e),
+        };
+        }
+    }
+    out_handle.write(cursor::Goto(4, 2));
+}
 
-
-
+// #[cfg(unix)]
+fn get_cursor_pos() {
+    //println!("fuck");
+    cursor::Goto(4, 2);
+}
